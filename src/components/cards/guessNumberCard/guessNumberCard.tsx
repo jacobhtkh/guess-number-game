40fcb1f6a1513464e0a1f89e3cc3b6190dad475a
Jacob Ujanga <jacob.ujanga@sero.life>
2024-03-26 19:57:49 +0000

    Start guessing logic

--- src/components/cards/guessNumberCard/guessNumberCard.tsx
+++ src/components/cards/guessNumberCard/guessNumberCard.tsx
@@ -1,21 +1,47 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { PrimaryButton } from '../../buttons/primaryButton';
 
 export const GuessNumberCard: React.FunctionComponent = () => {
+  const [hasGameStarted, setHasGameStarted] = useState(false);
+
+  useEffect(() => {
+    setHasGameStarted(false);
+  }, []);
+
+  const onClickStartGuess = () => {
+    setHasGameStarted(true);
+  };
+
   return (
     <div className='max-w-md border border-solid rounded-md border-sky-600 shadow-md shadow-sky-600'>
       <div className='px-6 py-4 text-center'>
-        <h2 className=' mb-3 text-xl font-semibold tracking-tight text-sky-600'>
-          Guess My Number!
-        </h2>
-        <h4 className='mb-2 text-sky-900'>
-          Think of a number between 1 and 10,000.
-        </h4>
-        <p className='mb-5'>
-          Click the button below to get the computer to start guessing what it
-          is.
-        </p>
-        <PrimaryButton text='Start guessing' onClick={() => {}} />
+        {!hasGameStarted ? (
+          <div>
+            <h2 className=' mb-3 text-xl font-semibold tracking-tight text-sky-600'>
+              Guess My Number!
+            </h2>
+            <h4 className='mb-2 text-sky-900'>
+              Think of a number between 1 and 10,000.
+            </h4>
+            <p className='mb-5'>
+              Click the button below to get the computer to start guessing what
+              it is.
+            </p>
+          </div>
+        ) : (
+          <div className='mb-5'>
+            <h4 className='text-sky-900'>Did I guess correctly?</h4>
+          </div>
+        )}
+        {!hasGameStarted ? (
+          <PrimaryButton text='Start guessing' onClick={onClickStartGuess} />
+        ) : (
+          <div className='flex justify-between space-x-6'>
+            <PrimaryButton text='Too low' onClick={onClickStartGuess} />
+            <PrimaryButton text='Correct' onClick={onClickStartGuess} />
+            <PrimaryButton text='Too high' onClick={onClickStartGuess} />
+          </div>
+        )}
       </div>
     </div>
   );
